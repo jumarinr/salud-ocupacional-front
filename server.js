a18cf8634093e0a797effb8748b53c6bb3f50556
jumarinr <jumarinr@unal.edu.co>
2019-11-11 09:49:14 -0500

    Revert "añadimos cors a ver que tal"
    This reverts commit c9c4893f253bcf0d87b1c9033351772b33d6af56.

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 
 const port = process.env.PORT || 8080
 
-const cors = require('cors');
-
 
 const app = express()
-
-app.use(cors({credentials: true}))
 
 app.use(express.static(__dirname + '/dist/'));
 
