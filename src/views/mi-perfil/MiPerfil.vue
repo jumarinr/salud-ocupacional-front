b20af96600bcc541dfc341cedc41cef441717d42
Scabrerax <scabrera@unal.edu.co>
2020-01-31 15:06:29 -0500

    Recepcion de información de la db

--- src/views/mi-perfil/MiPerfil.vue
+++ src/views/mi-perfil/MiPerfil.vue
@@ -78,7 +78,7 @@ export default {
   },
   data() {
     return {
-      trabajador: {},
+      trabajador: JSON.parse(localStorage.getItem("usertoken")).datos,
        // ----- Datos de la tabla
       camposVacunas: [
         {key:"nombre", sortable: true}, 
